e35c4678db497ac9227d89ac64aaf62872d5ca3d
sajjad <s.hassanpourk@gmail.com>
2019-11-11 12:53:55 +0100

    dark primary color// change app title

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:hundred_climbs/src/assets-content/colors/AppColors.dart';
 import 'package:hundred_climbs/src/dashboard/state/dashboardState.dart';
 import 'package:hundred_climbs/src/services/auth.dart';
 import 'package:hundred_climbs/src/home.dart';
@@ -7,9 +10,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() async {
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
   runApp(HundredClimbs());
 }
 
@@ -20,7 +26,9 @@ class HundredClimbs extends StatelessWidget {
     return StreamProvider<FirebaseUser>.value(
       stream: FirebaseAuth.instance.onAuthStateChanged,
       child: MaterialApp(
-        title: 'CLIMBING LOGBOOK',
+        debugShowCheckedModeBanner: false,
+        title: '100 Climbs',
+        theme: ThemeData(primaryColor: AppColors.dark),
         home: Consumer<FirebaseUser>(
           builder: (context, firebaseUser, _) {
             if (firebaseUser != null) {
