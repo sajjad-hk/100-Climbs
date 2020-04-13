e975f578d6508f892b45626d16a8f45646514f67
sajjad <s.hassanpourk@gmail.com>
2020-04-13 19:55:25 +0200

    Accessibility fixes

--- lib/src/screens/intro-screen/intro-screen.dart
+++ lib/src/screens/intro-screen/intro-screen.dart
@@ -21,115 +21,118 @@ class _IntroState extends State<Intro> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            AppColors.lightNavy,
-            AppColors.dark,
-          ],
-        ),
-      ),
-      child: Stack(
-        alignment: AlignmentDirectional.center,
-        children: <Widget>[
-          PageView(
-            controller: _controller,
-            onPageChanged: (int i) {
-              setState(() {
-                currentPageValue = i;
-              });
-            },
-            children: <Widget>[
-              IntroPage(
-                ind: 1,
-                emailTapRecognizer: TapGestureRecognizer()
-                  ..onTap = () => sendEmail(),
-                privacyLinkTapRecognizer: TapGestureRecognizer()
-                  ..onTap = () => _launchURL(),
-              ),
-              IntroPage(
-                ind: 2,
-                emailTapRecognizer: TapGestureRecognizer()
-                  ..onTap = () => sendEmail(),
-                privacyLinkTapRecognizer: TapGestureRecognizer()
-                  ..onTap = () => _launchURL(),
-              ),
-              IntroPage(
-                ind: 3,
-                emailTapRecognizer: TapGestureRecognizer()
-                  ..onTap = () => sendEmail(),
-                privacyLinkTapRecognizer: TapGestureRecognizer()
-                  ..onTap = () => _launchURL(),
-              ),
-              IntroPage(
-                ind: 4,
-                emailTapRecognizer: TapGestureRecognizer()
-                  ..onTap = () => sendEmail(),
-                privacyLinkTapRecognizer: TapGestureRecognizer()
-                  ..onTap = () => _launchURL(),
-              )
+    return Semantics(
+      label: 'Intro screen page views',
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              AppColors.lightNavy,
+              AppColors.dark,
             ],
           ),
-          Positioned(
-            bottom: screens['INTRO']['MARGIN'][LayoutUtils(context).screenSize],
-            child: Semantics(
-              readOnly: true,
-              label: 'Sign in with Google account',
-              button: true,
-              child: Column(
-                children: <Widget>[
-                  Stack(
-                    alignment: AlignmentDirectional.topStart,
-                    children: <Widget>[
-                      Container(
-                        margin: screens['INTRO']['P_I']
-                            [LayoutUtils(context).screenSize],
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            for (int i = 0; i < 4; i++)
-                              if (i == currentPageValue) ...[
-                                circleBar(true, i)
-                              ] else
-                                circleBar(false, 0),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  Semantics(
-                    label: 'Sign in with Google',
-                    button: true,
-                    child: FlatButton.icon(
-                      padding: screens['INTRO']['BUTTON']
-                          [LayoutUtils(context).screenSize],
-                      color: AppColors.dark,
-                      icon: Image(
-                        height: 30,
-                        width: 30,
-                        image: AssetImage(AppIcons.google),
-                      ),
-                      label: Text(
-                        'Sign in with Google',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () async {
-                        FirebaseUser user = await authService.googleSignIn();
-                        if (user != null) {
-                          Navigator.pushReplacementNamed(context, '/');
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
+        ),
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            PageView(
+              controller: _controller,
+              onPageChanged: (int i) {
+                setState(() {
+                  currentPageValue = i;
+                });
+              },
+              children: <Widget>[
+                IntroPage(
+                  ind: 1,
+                  emailTapRecognizer: TapGestureRecognizer()
+                    ..onTap = () => sendEmail(),
+                  privacyLinkTapRecognizer: TapGestureRecognizer()
+                    ..onTap = () => _launchURL(),
+                ),
+                IntroPage(
+                  ind: 2,
+                  emailTapRecognizer: TapGestureRecognizer()
+                    ..onTap = () => sendEmail(),
+                  privacyLinkTapRecognizer: TapGestureRecognizer()
+                    ..onTap = () => _launchURL(),
+                ),
+                IntroPage(
+                  ind: 3,
+                  emailTapRecognizer: TapGestureRecognizer()
+                    ..onTap = () => sendEmail(),
+                  privacyLinkTapRecognizer: TapGestureRecognizer()
+                    ..onTap = () => _launchURL(),
+                ),
+                IntroPage(
+                  ind: 4,
+                  emailTapRecognizer: TapGestureRecognizer()
+                    ..onTap = () => sendEmail(),
+                  privacyLinkTapRecognizer: TapGestureRecognizer()
+                    ..onTap = () => _launchURL(),
+                )
+              ],
             ),
-          )
-        ],
+            Positioned(
+              bottom: screens['INTRO']['MARGIN'][LayoutUtils(context).screenSize],
+              child: Semantics(
+                readOnly: true,
+                label: 'page indicator',
+                button: true,
+                child: Column(
+                  children: <Widget>[
+                    Stack(
+                      alignment: AlignmentDirectional.topStart,
+                      children: <Widget>[
+                        Container(
+                          margin: screens['INTRO']['P_I']
+                              [LayoutUtils(context).screenSize],
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              for (int i = 0; i < 4; i++)
+                                if (i == currentPageValue) ...[
+                                  circleBar(true, i)
+                                ] else
+                                  circleBar(false, 0),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Semantics(
+                      label: 'Sign in with Google',
+                      button: true,
+                      child: FlatButton.icon(
+                        padding: screens['INTRO']['BUTTON']
+                            [LayoutUtils(context).screenSize],
+                        color: AppColors.dark,
+                        icon: Image(
+                          height: 30,
+                          width: 30,
+                          image: AssetImage(AppIcons.google),
+                        ),
+                        label: Text(
+                          'Sign in with Google',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          FirebaseUser user = await authService.googleSignIn();
+                          if (user != null) {
+                            Navigator.pushReplacementNamed(context, '/');
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
